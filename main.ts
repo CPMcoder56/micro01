2f48ffbb0e04562ceea9e2b8c1ce4c9531369e39
CPMcoder56 <matthieu.laulier@gmail.com>
2025-01-05 11:32:06 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+cuteBot.colorLight(cuteBot.RGBLights.ALL, 0xb09eff)
+basic.showIcon(IconNames.Happy)
+music.play(music.stringPlayable("C E E D A E G D ", 290), music.PlaybackMode.UntilDone)
+basic.pause(100)
+cuteBot.moveTime(cuteBot.Direction.forward, 60, 5)
+basic.pause(5000)
+cuteBot.stopcar()
 basic.forever(function () {
 	
 })
